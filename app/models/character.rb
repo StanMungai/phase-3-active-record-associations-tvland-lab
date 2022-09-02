c7cca4e70dc215721c60c11b75fd1465f7694a60
stanford kangethe <stanfordkangethe16@gmail.com>
2022-09-02 23:59:17 +0300

    partly done

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor 
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{actor.first_name} #{actor.last_name} always says: #{self.catchphrase}"
+  end
 end
