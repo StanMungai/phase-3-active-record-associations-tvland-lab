bbd921e037e381bbb69ad72de6a0f0f2931df275
stanford kangethe <stanfordkangethe16@gmail.com>
2022-11-06 07:52:19 +0300

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,9 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     # list all the characters
-    self.characters.each {|character| puts}
+    self.characters.map do |character| 
+      "#{character.name} - #{character.show.name}"
+    end
     # list all shows
     # self.shows
   end
